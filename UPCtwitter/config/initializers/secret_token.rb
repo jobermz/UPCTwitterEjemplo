b70818fe4c552c717733066e540e08c0d5f9a2c3
UPC_SEDE_SAN_BORJA <spantigo@upc.edu.pe>
2013-05-28 21:32:07 -0500

    Primer envio

---
+++ UPCtwitter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UPCtwitter::Application.config.secret_token = '7af486096a89ec1915a7c83a82716f122556e0f5899ef21c9b8dac42ad90bfaf56523fe9d0bb29f43ef7120663c216863c4aa9b0c84c1b1431357201f4d3187e'
